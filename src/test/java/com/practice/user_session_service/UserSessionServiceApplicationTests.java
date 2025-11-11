fe33f4e4eff9ce6d86b40c2b0c7155c8a0ad7292
Mahnoor Abbasi <mahnoorabbasi>
2025-11-11 12:53:36 +0400

    init

---
+++ src/test/java/com/practice/user_session_service/UserSessionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.user_session_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSessionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
